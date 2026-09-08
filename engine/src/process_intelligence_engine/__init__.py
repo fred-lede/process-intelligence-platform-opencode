25c49763ee9cda268a83d1df1a4a97730157276b
fred-lede <fred.yt.wang@outlook.com>
2026-09-09 04:35:53 +0700

    release: advance platform to v0.6.0

--- engine/src/process_intelligence_engine/__init__.py
+++ engine/src/process_intelligence_engine/__init__.py
@@ -4,4 +4,4 @@ This package contains the data import, analysis, modeling, simulation
 and reporting modules that power the platform's statistical engine.
 """
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
