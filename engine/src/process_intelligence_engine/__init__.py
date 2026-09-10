adf918404d4b6518c46a80378407b883a6fce2a1
fred-lede <fred.yt.wang@outlook.com>
2026-09-11 06:15:45 +0700

    sync v0.8.3 across engine and app

--- engine/src/process_intelligence_engine/__init__.py
+++ engine/src/process_intelligence_engine/__init__.py
@@ -4,4 +4,4 @@ This package contains the data import, analysis, modeling, simulation
 and reporting modules that power the platform's statistical engine.
 """
 
-__version__ = "0.8.2"
+__version__ = "0.8.3"
